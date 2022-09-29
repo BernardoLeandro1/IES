9bb789e0ee13b8f9d0195ab0179bd251c700c0af
Bernardo Leandro <bernardoleandro1@ua.pt>
2022-09-29 14:46:31 +0100

    lab1 complete

--- lab1/README.md
+++ lab1/README.md
@@ -13,14 +13,17 @@ In lab1.2 we create and explore our first maven project of this class.
         In the pom.xml file I inserted developer, changed the java version and inserted character encoder
 
 In lab1.3 we learn about git, add .gitignore files to the projects.
-In this exercise I wasn't really sure what was asked, in d) e) f)
+
+    In this exercise i cloned my repo in location2 and made some changes
+
+In lab1.4 we learned about docker and some important commands. 
+
+We learned what is a Dockerfile (text document that contains all the commands a user could call on the command line to assemble an image) and a docker-compose.yml file (you use a YAML file to configure your application's services)
 
 
+In lab 1.5, I needed to put the artifact of ipmaclient on the pom.xml of weatherforecast 
 
-
-
-
-
+During this lab I also made a general notebook, which will be updated in every lab, that contains the most important/useful commands.
 
 
 From now on, groupID = labX and artifactID = exY, X being the lab number and Y being the exercise number from labX
---
+++ lab1/lab1_5/weatherforecast/src/main/java/lab1_5/WeatherStarter.java
@@ -0,0 +1,64 @@
+package lab1_5;
+
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import java.lang.StringBuilder;
+
+import lab1_5.CityForecast;
+/**
+ * demonstrates the use of the IPMA API for weather forecast
+ */
+public class WeatherStarter {
+
+    //todo: should generalize for a city passed as argument
+    private static final int CITY_ID_AVEIRO = 1010500;
+
+    public static void  main(String[] args ) {
+
+        // get a retrofit instance, loaded with the GSon lib to convert JSON into objects
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://api.ipma.pt/open-data/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        // create a typed interface to use the remote API (a client)
+        IpmaService service = retrofit.create(IpmaService.class);
+        // prepare the call to remote endpoint
+        int cityID = 1010500;
+
+        if (args.length > 0){
+            try {
+                cityID = Integer.parseInt(args[0]);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        Call<IpmaCityForecast> callSync = service.getForecastForACity(cityID);
+
+        try {
+            Response<IpmaCityForecast> apiResponse = callSync.execute();
+            IpmaCityForecast forecast = apiResponse.body();
+
+            if (forecast != null) {
+                StringBuilder sb = new StringBuilder();
+                String temps = sb.append("Maximum temperature for today: ")
+                                .append(forecast.getData().listIterator().next().getTMax())
+                                .append("C.")
+                                .append('\n')
+                                .append("Minimum temperature for today: ")
+                                .append(forecast.getData().listIterator().next().getTMin())
+                                .append("C.")
+                                .toString();
+                System.out.print(temps);
+            } else {
+                System.out.println( "No results for this request!");
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+    }
+}
